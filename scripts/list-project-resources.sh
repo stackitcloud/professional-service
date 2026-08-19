bad5524e05241831cdeec3ad1136641f60f23946
Mauritz Uphoff <mauritz.uphoff@stackit.cloud>
2026-08-19 14:47:12 +0200

    license: add header to shell scripts

--- scripts/list-project-resources.sh
+++ scripts/list-project-resources.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+# Copyright 2026 Schwarz Digits Cloud GmbH & Co. KG
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 
 # Script to list (all) resources of a STACKIT project
 # Ideally, you redirect the output into a markdown file.
